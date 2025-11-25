a10725eefd0cb8bf2031e83d925908fb87f14833
BrexR01Z <breyespro@gmail.com>
2025-11-25 18:54:45 -0300

    crear est

--- cuentas/admin.py
+++ cuentas/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from .models import Usuario,Cliente,Dueno
 
 # Register your models here.
 
+admin.site.register(Usuario)
+admin.site.register(Cliente)
+admin.site.register(Dueno)
