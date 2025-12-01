308c3f2224e5d101a8c503ba1d37eafbae05232d
BrexR01Z <breyespro@gmail.com>
2025-11-30 21:37:49 -0300

    crear reservas muestra reservas existentes x cancha

--- reservas/views.py
+++ reservas/views.py
@@ -5,52 +5,60 @@ from cuentas.models import Usuario,Dueno
 from .forms import CrearReservaForm
 from django.contrib import messages
 from datetime import datetime, timedelta
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
+@login_required(login_url='cuentas:login_cuenta')
+@require_http_methods(["GET", "POST"])
 def crear_reserva(request, cancha_id):
     cancha = get_object_or_404(Cancha, id=cancha_id)
-
-    if request.method == "POST" : 
+    
+    reservas_cancha = Reserva.objects.filter(
+        cancha=cancha,
+        estado__in=['PENDIENTE', 'CONFIRMADA']
+    ).select_related('usuario').order_by('fecha', 'hora_inicio')
+    
+    if request.method == 'POST':
         form = CrearReservaForm(request.POST)
         if form.is_valid():
             reserva = form.save(commit=False)
             reserva.cancha = cancha
             reserva.usuario = request.user
-
-            if revisar_horario(reserva):
-                messages.error(request, "Ya existe una reserva en ese horario")
-            else:                
+            
+            if conflicto_hora(reserva):
+                messages.error(request, 'Ya existe una reserva en ese horario.')
+            else:
                 reserva.save()
-                messages.success(request,"Reserva creada exitosamente")
-                return redirect ("cuentas:home")
-                
-            # sugerir mandar invitaciones luego de hacer reserva exitosa
-                # return redirect("MOSTRAR DETALLE RESERVA, HACER", id=reserva.id)
+                messages.success(request, 'Reserva creada exitosamente!')
+                return redirect('cuentas:home')
+                #return redirect('reservas:ver_reserva', id=reserva.id)
     else:
         form = CrearReservaForm()
+    
+    for reserva in reservas_cancha:
+        reserva.hora_fin = reserva.hora_fin()
+    
+    return render(request, 'crear_reserva.html', {
+        'form': form,
+        'cancha': cancha,
+        'reservas_cancha': reservas_cancha,
+    })
 
-    context = {
-        "form" : form,
-        "cancha" : cancha,
-    }
 
-    return render (request, "crear_reserva.html", context)
-
-
-def revisar_horario(reserva):
+def conflicto_hora(reserva):
+    
     inicio = datetime.combine(reserva.fecha, reserva.hora_inicio)
     fin = inicio + timedelta(minutes=reserva.duracion_bloques * 30)
-
+    
     conflictos = Reserva.objects.filter(
         cancha=reserva.cancha,
         fecha=reserva.fecha,
-        estado__in=['PENDIENTE', 'CONFIRMADA']
-    ).exclude(id=reserva.id)
+        estado__in=['PENDIENTE', 'CONFIRMADA']).exclude(id=reserva.id)
     
     for x in conflictos:
         x_fin = datetime.combine(x.fecha, x.hora_fin())
         if not (fin.time() <= x.hora_inicio or reserva.hora_inicio >= x_fin.time()):
             return True
     return False
-
