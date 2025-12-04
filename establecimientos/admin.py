deb942a59a1d6949e0db1cd890e2eaf104ec47b0
aconstanzov <antonioconstanzovicens@gmail.com>
2025-12-04 01:51:56 -0300

    Delete establecimientos directory

--- establecimientos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Cancha,Establecimiento,HorarioEstablecimiento
-# Register your models here.
-admin.site.register(Cancha)
-admin.site.register(Establecimiento)
-admin.site.register(HorarioEstablecimiento)
